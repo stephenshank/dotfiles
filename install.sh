243056c93b74fe674b992c213cec6ebc149a46e7
Stephen Shank <sshank314@gmail.com>
2019-05-09 11:33:02 -0400

    Move to neovim and deoplete.

--- install.sh
+++ install.sh
@@ -3,10 +3,9 @@ rm -f ~/.tmux.conf; ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 # VIM
 ## Vundle
+rm -rf ~/.vim; ln -s ~/dotfiles/.vim ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-rm -f ~/.vimrc ; ln -s ~/dotfiles/.vimrc ~/.vimrc
-vim +PluginInstall +qall
-
-# TODO: clang-completer is not working on Ubuntu
-## YouCompleteMe: https://valloric.github.io/YouCompleteMe/
-/usr/bin/python3 ~/.vim/bundleplugged/YouCompleteMe/install.py --clang-completer
+rm -rf ~/.config/nvim
+mkdir ~/.config/nvim
+rm -f ~/.vimrc ; ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
+nvim +PluginInstall +qall
