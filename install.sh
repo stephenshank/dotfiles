a0c1e0fefbf9c76a278a7a4e802405b2201f0487
Stephen Shank <sshank314@gmail.com>
2019-06-03 17:42:32 -0400

    Setup for tern.

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ git submodule update --init
 rm -f ~/.tmux.conf; ln -s $PWD/.tmux.conf ~/.tmux.conf
 
 ## Neovim
-npm install -g tern
+npm install -g tern neovim
 rm -rf ~/.config/nvim; ln -s $PWD/nvim ~/.config/nvim
+rm ~/.tern-config; ln -s $PWD/.tern-config ~/.tern-config
 cd neovim
 make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/Software"
 make install
